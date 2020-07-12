0a10fb0a1d839724efc9c8e56f3861ab38cfde9a
Aaron Fabricio <aaronfabricio00@gmail.com>
2020-07-12 19:18:29 -0400

    primer commit header footer y baner

---
+++ public/js/index.js
@@ -0,0 +1,6 @@
+const menu = document.getElementById('menu')
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+menu.addEventListener('click', ()=>{
+    title.classList.add('text-red-200')
+})
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  theme: {
+    
+    extend: {
+      colors: {
+        'primary':'#f3c614',
+        'secondary':'#353535'
+      },
+      height:{
+        '100vh': "100vh",
+        '80vh': "80vh",
+        '50vh': "50vh"
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
