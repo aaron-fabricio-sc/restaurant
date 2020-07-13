9da44a4c54b775b981c4fdd1a56203a073af07ea
Aaron Fabricio <aaronfabricio00@gmail.com>
2020-07-13 18:59:21 -0400

    primera version

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,16 @@
 const autoprefixer = require('autoprefixer');
 
+// const purgecss = require('@fullhuman/postcss-purgecss')
 module.exports = {
     plugins : [
         require('tailwindcss'),
         require('autoprefixer'),
+        // purgecss({
+        //     content: ['./**/*.html']
+        //   }),
+        require('cssnano')({
+            preset: 'default',
+        }),
     ]
 }
 
